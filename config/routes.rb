04e390cb015fd9a211b948a93ea83bbb22492cb9
jamvar <varma.santosh@icloud.com>
2021-03-21 17:57:07 -0500

    update route for testing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     end
   end
 
+  root to: 'home#index'
+
   post 'refresh', controller: :refresh, action: :create
   post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
